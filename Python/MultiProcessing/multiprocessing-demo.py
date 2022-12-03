63863a6163c86eb7998ef9589f972e186d4eb5ad
George Khananaev <106206490+georgekhananaev@users.noreply.github.com>
2022-12-03 17:02:53 +0200

    throws errors in windows 11 with python 3.11+
    you should run your processes pool inside if __name__ == '__main__':

--- Python/MultiProcessing/multiprocessing-demo.py
+++ Python/MultiProcessing/multiprocessing-demo.py
@@ -1,22 +1,45 @@
-import concurrent.futures
 import time
+import concurrent.futures
+from PIL import Image, ImageFilter
 
-start = time.perf_counter()
+img_names = [
+    'photo-1516117172878-fd2c41f4a759.jpg',
+    'photo-1532009324734-20a7a5813719.jpg',
+    'photo-1524429656589-6633a470097c.jpg',
+    'photo-1530224264768-7ff8c1789d79.jpg',
+    'photo-1564135624576-c5c88640f235.jpg',
+    'photo-1541698444083-023c97d3f4b6.jpg',
+    'photo-1522364723953-452d3431c267.jpg',
+    'photo-1513938709626-033611b8cc03.jpg',
+    'photo-1507143550189-fed454f93097.jpg',
+    'photo-1493976040374-85c8e12f0c0e.jpg',
+    'photo-1504198453319-5ce911bafcde.jpg',
+    'photo-1530122037265-a5f1f91d3b99.jpg',
+    'photo-1516972810927-80185027ca84.jpg',
+    'photo-1550439062-609e1531270e.jpg',
+    'photo-1549692520-acc6669e2f0c.jpg'
+]
 
 
-def do_something(seconds):
-    print(f'Sleeping {seconds} second(s)...')
-    time.sleep(seconds)
-    return f'Done Sleeping...{seconds}'
+
+size = (1200, 1200)
 
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    secs = [5, 4, 3, 2, 1]
-    results = executor.map(do_something, secs)
+def process_image(img_name):
+    img = Image.open(img_name)
 
-    # for result in results:
-    #     print(result)
+    img = img.filter(ImageFilter.GaussianBlur(15))
 
-finish = time.perf_counter()
+    img.thumbnail(size)
+    img.save(f'processed/{img_name}')
+    print(f'{img_name} was processed...')
 
-print(f'Finished in {round(finish-start, 2)} second(s)')
+
+if __name__ == '__main__':
+    t1 = time.time()
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(process_image, img_names)
+
+    t2 = time.time()
+    print(f'Finished in {t2 - t1} seconds')
